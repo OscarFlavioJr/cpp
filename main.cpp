de257384a51ada0ed485939e98d1a111893efa45
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2024-12-10 09:31:36 -0300

    CPP

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int soma;
+    std::cout<<"1 plus 1 is equal to? "<<std::endl;
+    std::cin>>soma;
+
+    while(soma !=2){
+        std::cout<<"You're really dumbass! Try again bitch"<<std::endl;
+        std::cin>>soma;
+    }
+
+    std::cout<<"Congrats, 1 plus one is equal to TWO"<<std::endl;
+
+    system("PAUSE");
+}
